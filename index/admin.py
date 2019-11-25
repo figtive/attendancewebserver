e13a9ed09b17dfa5cde1be55dd0551d59c524704
aufawiandra <aufawiandra@gmail.com>
2019-11-25 15:54:43 +0700

    Registered models into admin

--- index/admin.py
+++ index/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+
+admin.site.register(Students)
+admin.site.register(Courses)
+admin.site.register(Classes)
+admin.site.register(Attendance)
--- index/models.py
+++ index/models.py
@@ -14,7 +14,7 @@ class Classes(models.Model):
     class_name = models.ForeignKey(Courses, on_delete=models.CASCADE)
     time_start = models.TimeField()
     time_end = models.TimeField()
-
+ 
 class Attendance(models.Model):
     atendee = models.ForeignKey(Students, on_delete=models.CASCADE)
     class_attend = models.ForeignKey(Classes, on_delete=models.CASCADE)
