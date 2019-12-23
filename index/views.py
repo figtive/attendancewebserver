3f0a9512b63581a65f3fac7cae43041ea3cd0f63
andreSatria <andresatria215@gmail.com>
2019-12-23 18:43:54 +0700

    Added multiple urls

--- index/views.py
+++ index/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.views.generic import TemplateView
-from .models import Attendance, Course
+from .models import Attendance, Course, Meeting,Lecturer
+from django.http import HttpResponse
 
 def index(request):
     attendances = Attendance.objects.all()
@@ -8,4 +9,36 @@ def index(request):
         'attendances': attendances,
     })
 
+def showMeeting(request, course_code):
+    meetings = Meeting.objects.filter(course_class__course__code=course_code)
+    course = Course.objects.get(code=course_code)
+    print(meetings)
+    return render(request, 'meetingsList.html', {
+        'meetings': meetings,
+        'course': course
+
+    })
+
+def showAttendance(request,course_code, pk):
+    students = Attendance.objects.all().filter(meeting__pk=pk)
+    meeting = Meeting.objects.get(pk=pk)
+    return render(request, 'attendanceList.html', {
+        'students': students,
+        'meeting': meeting,
+    })
+
+def showLecturerCourses(request,lecturer_npm):
+    courses = Course.objects.all().filter(lecturer__npm= lecturer_npm)
+    lecturer = Lecturer.objects.get(npm=lecturer_npm)
+    return render(request, 'lecturerCourseList.html', {
+        "courses": courses,
+        "lecturer": lecturer
+    })
+
+
+def showLecturer(request):
+    lecturers = Lecturer.objects.all()
+    return render(request, 'lecturersList.html', {
+        "lecturers": lecturers
+    })
 
