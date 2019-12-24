7786c58e5f76e4124e72cf04c0050d7f3c9da291
Nicolaus C <nicolauscg@gmail.com>
2019-12-24 12:14:06 +0700

    complete attendance flow for student and lecturer

--- nfc/nfc.py
+++ nfc/nfc.py
@@ -23,11 +23,15 @@ sys.path.insert(0, os.environ["BASE_DIR"])
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "attendancewebserver.settings")
 django.setup()
 
+from django.utils import timezone
 from index.models import *
 
 logging_format = "%(asctime)s: %(message)s"
 logging.basicConfig(format=logging_format, level=logging.INFO, datefmt="%H:%M:%S")
 new_lock = threading.Lock()
+
+def get_current_date_time():
+    return timezone.localtime(timezone.now())
 
 class SystemState:
     IDLE = 0
@@ -43,17 +47,22 @@ class SystemState:
         self.check_still_on_going()
         return self.current_state == SystemState.ON_GOING_CLASS
     def set_idle(self):
+        logging.info('system state set to idle')
         self.current_state = SystemState.IDLE
         self.end_class_date_time = None
         self.meeting = None
     def set_on_going_class(self, meeting, end_class_date_time):
+        logging.info('system state set to on going class ending on {}'.format(end_class_date_time))
         self.current_state = SystemState.ON_GOING_CLASS
         self.end_class_date_time = end_class_date_time
         self.meeting = meeting
     def check_still_on_going(self):
         if self.end_class_date_time is not None and self.current_state == SystemState.ON_GOING_CLASS:
-            if datetime.now() > self.end_class_date_time:
+            if get_current_date_time() > self.end_class_date_time:
+                logging.info('system state will set to idle as {} finished', self.meeting)
                 self.set_idle()
+    def get_meeting(self):
+        return self.meeting
 
 class Keypad:
     MAPPING = {
@@ -100,6 +109,7 @@ class Keypad:
                     return Keypad.REVERSE_MAPPING[pressed_key]
 
 def start_nfc_poll_producer(queue_to_put):
+    logging.info('nfc poll producer started')
     while True:
         process = subprocess.Popen(['unbuffer', 'nfc-poll'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         stdout = AsynchronousFileReader(process.stdout, autostart=True)
@@ -117,10 +127,12 @@ def start_nfc_poll_producer(queue_to_put):
         sys_time.sleep(.1)
 
 def start_nfc_poll_consumer(queue_to_read):
+    logging.info('nfc poll consumer started')
     keypad = Keypad()
     lcd = Lcd()
     led_buzzer = LedBuzzer()
     system_state = SystemState()
+    lcd.write(["attendance", "system"])
     while True:
         nfc_output_line = queue_to_read.get()
         uid_line_match = re.search("UID.*\\n", nfc_output_line)
@@ -128,36 +140,78 @@ def start_nfc_poll_consumer(queue_to_read):
             continue
         record = Record.objects.create(payload=uid_line_match.group())
         uid = re.sub('( |\\n)', '',  uid_line_match.group().split(':')[1])
-        print("read uid {}".format(uid))
+        logging.info("read uid {}".format(uid))#CHECKED
         if system_state.is_idle():
+            logging.info("system idle")
             lecturer = Lecturer.objects.all().filter(serial_number=uid)
             if not lecturer.exists():
+                logging.info("lecturer not found")#CHECKED
                 lcd.write(["invalid", "lecturer card"])
+                led_buzzer.trigger_failure()
                 continue
             else:
+                logging.info("lecturer {} found".format(repr(lecturer[0])))
                 lcd.write(["0 normal", "1 substitute"])
                 class_input = keypad.read("01")
                 if class_input == "0":
-                    course_class = get_nearest_course_class_of_lectuerer()
+                    course_class = get_nearest_course_class_of_lectuerer(lecturer[0], record.get_date_time())
+                    if course_class is None:#CHECKED
+                        lcd.write(["no class", "found for today"])
+                        led_buzzer.trigger_failure()
+                        continue
                     meeting = Meeting.objects.create( \
                         course_class=course_class, \
                         record=record, meeting_type="0")
-                else:
-                    course = menu(lcd, keypad, \
-                        Course.objects.all().filter(lecturer=lecturer), lambda e: e.name)
-                    course_class = menu(lcd, keypad, \
+                else:#CHECKED
+                    course = choice_menu(lcd, keypad, \
+                        Course.objects.all().filter(lecturer=lecturer[0]), lambda e: e.name)
+                    course_class = choice_menu(lcd, keypad, \
                         CourseClass.objects.all().filter(course=course), \
                         lambda e: "{} {}".format(e.get_day_display(), str(e.start_time)))
                     meeting = Meeting.objects.create( \
                         course_class=course_class, \
                         record=record, meeting_type="1")
-                system_state.set_on_going_class(meeting, record.date_time + \
-                    datetime.combine(date.min, course_class.end_time) - datetime.combine(date.min, course_class.start_time))
+                logging.info("meeting {} created".format(repr(meeting)))
+                lcd.write(["{} {}".format(meeting.course_class.course.name[:12], meeting.course_class.get_day_display()), \
+                    "{} {}".format(meeting.course_class.start_time, meeting.get_meeting_type_display()[:4])])
+                led_buzzer.trigger_success()
+                system_state.set_on_going_class(meeting, record.get_date_time() + \
+                    (datetime.combine(date.min, course_class.end_time) - datetime.combine(date.min, course_class.start_time)))
+        else:
+            logging.info("system have on going class")
+            lecturer = Lecturer.objects.all().filter(serial_number=uid)
+            if lecturer.exists():#CHECKED
+                logging.info("lecturer {} found".format(repr(lecturer[0])))
+                logging.info("ending class")
+                lcd.write(["class ended"])
+                system_state.set_idle()
+                continue
+            student = Student.objects.all().filter(serial_number=uid)
+            if not student.exists():
+                logging.info("student not found")
+                npm = int(input_menu(lcd, keypad, "enter npm", 10))
+                student = Student.objects.create(serial_number=uid, name="", npm=npm)
+                student.name = "Stud #{}".format(student.id)
+                student.save()
+                logging.info("student {} created".format(repr(student)))
+                attendance = Attendance(student=student, meeting=system_state.get_meeting(), record=record)
+                logging.info("attendance {} created".format(repr(attendance)))
+                lcd.write([student.name, str(student.npm)])
+                led_buzzer.trigger_success()
             else:
-                # TODO student
-                pass
+                logging.info("student {} found".format(repr(student[0])))
+                current_meeting = system_state.get_meeting()
+                if Attendance.objects.all().filter(student=student[0], meeting=current_meeting).exists():
+                    logging.info("duplicate attendance")
+                    lcd.write([student[0].name, "duplicate attend"])
+                    led_buzzer.trigger_failure()
+                else:
+                    attendance = Attendance.objects.create(student=student[0], meeting=current_meeting, record=record)
+                    logging.info("attendance {} created".format(repr(attendance)))
+                    lcd.write([student[0].name, str(student[0].npm)])
+                    led_buzzer.trigger_success()
 
-def menu(lcd, keypad, choices, display_func=lambda e: e):
+def choice_menu(lcd, keypad, choices, display_func=lambda e: e):
     selection = list(enumerate(choices))
     submenu_count = math.ceil(len(choices)/2)
     current_submenu_index = 0
@@ -166,28 +220,51 @@ def menu(lcd, keypad, choices, display_func=lambda e: e):
     while True:
         index = current_submenu_index*2
         first_line = "{} {}".format(selection[index][0], display_func(selection[index][1]))
-        seoncd_line = "{} {}".format(selection[index+1][0], display_func(selection[index+1][1])) \
+        second_line = "{} {}".format(selection[index+1][0], display_func(selection[index+1][1])) \
             if index + 1 < len(choices) else ""
-        lcd.write([first_line, seoncd_line])
+        lcd.write([first_line, second_line])
         keypad_input = keypad.read(keypad_keys)
         if keypad_input == "E" and current_submenu_index + 1 < submenu_count:
             current_submenu_index += 1
         if keypad_input == "B" and current_submenu_index - 1 >= 0:
             current_submenu_index -= 1
         if keypad_input in string.digits:
-            return choices[int(keypad_input)]
+            result = choices[int(keypad_input)]
+            logging.info("choice menu selects {}".format(result))
+            return result
 
-def get_nearest_course_class_of_lectuerer(lecturer):
-    day_of_week = str(datetime.now().weekday())
-    time = datetime.now().time()
+def input_menu(lcd, keypad, prompt_message, input_length):
+    answer=""
+    lcd.write([prompt_message])
+    while True:
+        keypad_input = keypad.read(string.digits + "BE")
+        if keypad_input in string.digits and len(answer) < 10:
+            answer += keypad_input
+            lcd.write([prompt_message, answer])
+        elif keypad_input == "B":
+            answer = answer[:-1]
+            lcd.write([prompt_message, answer])
+        elif keypad_input == "E" and len(answer) == input_length:
+            logging.info("input menu inputs {}".format(answer))
+            return answer
+
+def get_nearest_course_class_of_lectuerer(lecturer, date_time):
+    day_of_week = str(date_time.weekday())
+    time = date_time.time()
+    print("time", time)
     course_classes = CourseClass.objects.all().filter( \
         course__lecturer=lecturer, day=day_of_week)
-    
-    return min(course_classes, key=lambda el: \
-        datetime.combine(date.min, el.start_time) - \
-        datetime.combine(date.min, time))
+    if not course_classes.exists():
+        logging.info("no normal class found today")
+        return None
+    result = min(course_classes, key=lambda el: \
+        max(datetime.combine(date.min, el.start_time), datetime.combine(date.min, time)) - \
+        min(datetime.combine(date.min, el.start_time), datetime.combine(date.min, time)) )
+    logging.info("get nearest course class {}".format(repr(result)))
+    return result
 
 if __name__ == "__main__":
+    logging.info("nfc main func started")
     nfc_reader_output_queue = queue.Queue()
 
     threads = []
